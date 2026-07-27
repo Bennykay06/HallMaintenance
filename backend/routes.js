c52b0710765b2c04e15dac309d420af1b2843eac
ayitey1 <132293423+ayitey1@users.noreply.github.com>
2026-07-27 18:57:55 +0000

    Delete backend directory

--- backend/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { sendMail } = require("./controller");
-
-router.post("/send-email", sendMail);
-
-module.exports = router;
